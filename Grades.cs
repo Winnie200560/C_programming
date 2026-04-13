64ef54f05c9a57a7960b343caa3a7dde0deeb8aa
Winnie <epanovaalina@gmail.com>
2026-04-13 09:59:11 +0500

    Add files via upload

---
+++ Grades.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Grades
+{
+    private int subject1;
+    private int subject2;
+    private int subject3;
+
+    // конструктор
+    public Grades(int Subject1, int Subject2, int Subject3)
+    {
+        this.Subject1 = Subject1;
+        this.Subject2 = Subject2;
+        this.Subject3 = Subject3;
+    }
+
+    // констркутор копирования
+    public Grades(Grades grades)
+    {
+        this.Subject1 = grades.Subject1;
+        this.Subject2 = grades.Subject2;
+        this.Subject3 = grades.Subject3;
+    }
+
+    // свойства
+    public int Subject1
+    {
+        get 
+        { 
+            return this.subject1; 
+        }
+        set
+        {
+            if (value < 1 || value > 5)
+            {
+                Console.WriteLine("Ошибка: оценка должна быть от 1 до 5");
+            }
+            else
+            {
+                this.subject1 = value;
+            }
+        }
+    }
+
+    public int Subject2
+    {
+        get 
+        { 
+            return this.subject2; 
+        }
+        set
+        {
+            if (value < 1 || value > 5)
+            {
+                Console.WriteLine("Ошибка: оценка должна быть от 1 до 5");
+            }
+            else
+            {
+                this.subject2 = value;
+            }
+        }
+    }
+
+    public int Subject3
+    {
+        get 
+        { 
+            return this.subject3; 
+        }
+        set
+        {
+            if (value < 1 || value > 5)
+            {
+                Console.WriteLine("Ошибка: оценка должна быть от 1 до 5");
+            }
+            else
+            {
+                this.subject3 = value;
+            }
+        }
+    }
+
+    // метод, вычисляющий максимально из полей
+    public int Max()
+    {
+        int max = Subject1;
+
+        if (Subject2 > max)
+            max = Subject2;
+
+        if (Subject3 > max)
+            max = Subject3;
+
+        return max;
+    }
+
+    // перегрузка метода ToString
+    public override string ToString()
+    {
+        return  "Оценка 1: " + Subject1 + " " + "Оценка 2: " + Subject2 + " " + "Оценка 3: " + Subject3;
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Point
+{
+    // объявление полей
+    private double x;
+    private double y;
+
+    // конструктор
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    // метод, вычисляющий расстояние от одной точки до другой (результат - double)
+    public double DistanceTo(Point other)
+    {
+        double dx = other.x - this.x;
+        double dy = other.y - this.y;
+
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+
+    // ++ увеличивает x на 1
+    public static Point operator ++(Point p)
+    {
+        return new Point(p.x + 1, p.y);
+    }
+
+    // -- уменьшает x на 1
+    public static Point operator --(Point p)
+    {
+        return new Point(p.x - 1, p.y);
+    }
+
+    // явное приведение к int (целая часть x)
+    public static explicit operator int(Point p)
+    {
+        return (int)p.x;
+    }
+
+    // неявное приведение к double (y)
+    public static implicit operator double(Point p)
+    {
+        return p.y;
+    }
+
+    // расстояние
+    public static double operator +(Point a, Point b)
+    {
+        return a.DistanceTo(b);
+    }
+
+    // Point + int (увеличиваем x)
+    public static Point operator +(Point p, int value)
+    {
+        return new Point(p.x + value, p.y);
+    }
+
+    // int + Point (увеличиваем x)
+    public static Point operator +(int value, Point p)
+    {
+        return new Point(p.x + value, p.y);
+    }
+
+    public override string ToString()
+    {
+        return "точка (" + x  + ";"+ y+ ")";
+    }
+
+}
